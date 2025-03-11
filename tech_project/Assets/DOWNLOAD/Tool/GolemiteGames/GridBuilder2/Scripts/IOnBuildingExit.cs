339c1fbbd8cea569ce02e5da7d2080cdd535d70e
LEE SEUNG HUN <140816010+shshhaha@users.noreply.github.com>
2025-03-12 00:13:09 +0900

    init
    a

--- tech_project/Assets/DOWNLOAD/Tool/GolemiteGames/GridBuilder2/Scripts/IOnBuildingExit.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public interface IOnBuildingExit
-{
-    /// <summary>
-    /// Called when the building exits being hovered via the ObjectSelector class
-    /// </summary>
-    /// <param name="gameObject"></param>
-    public void OnBuildingExit(GameObject gameObject);
-}
